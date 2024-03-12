16411a5e5d572e4b35174b5b2106a029bd70ab82
Bryan Trejo <bryantrejo@Bryans-MacBook-Pro.local>
2024-03-12 12:36:45 -0400

    refactor: made code more organized

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from './/App'
 import './App.css'
 
+import Game from "./pages/Game/Game";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
-        <App />
+        <Game />
     </StrictMode>
 );
