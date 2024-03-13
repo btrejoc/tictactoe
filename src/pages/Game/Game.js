c137814aed8979dd7bbb289a0aa79f58dcb79da7
Bryan Trejo <bryantrejo@Bryans-MacBook-Pro.local>
2024-03-13 11:19:14 -0400

    defined props and their expected types for Board and Square components

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,7 +1,6 @@
 import {useState} from "react";
-import Board from '../../components/Board'
+import {Board} from "../../components/Board";
 import './Game.css'
-
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
